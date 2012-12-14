a6b60c9b968448838569dd731bc0ac901a6069ef
Bobur Umerkulov <b.umerkulov@gmail.com>
2012-12-15 00:43:27 +0500

    uncomment lib

--- lib/crecipes.rb
+++ lib/crecipes.rb
@@ -2,11 +2,10 @@ require "capistrano"
 require "capistrano/cli"
 require "capistrano-resque"
 
-#require 'crecipes/helpers'
-#Capistrano::Configuration::Namespaces::Namespace.class_eval do
-#  def capture(*args)
-#    parent.capture *args
-#  end
-#end
-#Dir[File.join(File.dirname(__FILE__), "crecipes/*.rb")].sort.each { |lib| require lib }
-#Dir[File.join(File.dirname(__FILE__), "crecipes/tasks/*.rb")].sort.each { |lib| require lib }
+Capistrano::Configuration::Namespaces::Namespace.class_eval do
+ def capture(*args)
+   parent.capture *args
+ end
+end
+Dir[File.join(File.dirname(__FILE__), "crecipes/*.rb")].sort.each { |lib| require lib }
+Dir[File.join(File.dirname(__FILE__), "crecipes/tasks/*.rb")].sort.each { |lib| require lib }
